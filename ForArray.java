f8b2d38ed16be1ade4d6d3750dbf8b58bfe519c7
John-zhang-122 <zh1061008729@qq.com>
2019-09-29 19:23:05 +0800

    Created a project 1

---
+++ ForArray.java
@@ -0,0 +1,9 @@
+public class ForArray{
+	public static void main(String[] args){
+		int[] array = new int[Math random()*10+1]；
+		for(int i=0;i<array.length;++i)
+			array[i]=(int)(Math random*100);
+		for(int vlaue;array)
+			System.outprintln(vlaue);
+	}
+}
